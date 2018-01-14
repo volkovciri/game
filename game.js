e7a84ab80cbfdd56fba5fff34d8faa8a4b5e6894
volkovciri <volkovciri@gmail.com>
2018-01-14 18:51:19 +0400

    2

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 game =
 {
+	a: {},
+
 	canvas:
 	{
 		load: function ()
@@ -16,7 +18,7 @@ game =
 				this.width = window.innerWidth;
 			}
 
-			canvas.context = canvas.getContext ('2d');
+			game.context = canvas.getContext ('2d');
 			game.canvas = canvas;
 			game.canvas.resize ();
 			window.document.body.appendChild (game.canvas);
@@ -25,6 +27,37 @@ game =
 
 	create:
 	{
+		animation: function (_)
+		{
+			let animation = game.create.sprite (_);
+				animation.a = _.a;
+				animation.step = _.step || 0;
+				animation.delay = _.delay || game.window.tick;
+				animation.time = _.time || game.window.time;
+
+			animation.animate = function ()
+			{
+				if (animation.loop ())
+				{
+					if (game.window.time - animation.time >= animation.delay)
+					{
+						animation.time = game.window.time;
+						animation.step = (animation.step >= animation.a.length - 1) ? 0 : animation.step + 1;
+						animation.i = animation.a[animation.step];
+						
+						animation.trace ();
+					}
+				}
+			}
+
+			animation.tick = function ()
+			{
+				animation.animate ();
+			}
+
+			return animation;
+		},
+
 		box: function (_)
 		{
 			let box = game.create.object (_);
@@ -33,8 +66,8 @@ game =
 
 				box.draw = function ()
 				{
-					if (box.color) { game.canvas.context.fillStyle = box.color; }
-					game.canvas.context.fillRect (box.x, box.y, box.w, box.h);
+					if (box.color) { game.context.fillStyle = box.color; }
+					game.context.fillRect (box.x, box.y, box.w, box.h);
 				}
 
 				box.set =
@@ -87,7 +120,22 @@ game =
 			}
 
 			return object;
-		}
+		},
+
+		sprite: function (_)
+		{
+			let sprite = game.create.box (_);
+				sprite.aa = _.aa || 0;
+				sprite.i = game.get.i (_.i);
+
+				sprite.draw = function ()
+				{
+					game.context.imageSmoothingEnabled = sprite.aa;
+					game.context.drawImage (sprite.i, sprite.x, sprite.y, sprite.w, sprite.h);
+				}
+
+			return sprite;
+		},
 	},
 
 	draw: function (redraw)
@@ -111,12 +159,51 @@ game =
 
 	get:
 	{
+		a: function (a)
+		{
+			let animation = (typeof (a) == 'object') ? a : game.a[a];
+				animation = (animation) ? animation : {};
+			return animation;
+		},
+
+		set animations (a)
+		{
+			for (id in a)
+			{
+				game.a[id] = [];
+				for (let i = 0; i < a[id]; i++)
+				{
+					let image = new Image ();
+						image.src = 'data/' + id + ' ' + i + '.png';
+						game.a[id].push (image);
+				}
+			}
+		},
+
 		boxinbox: function (a, b)
 		{
 			return ((Math.abs (a.x - b.x + 0.5 * (a.w - b.w)) < 0.5 * Math.abs (a.w + b.w)) && (Math.abs (a.y - b.y + 0.5 * (a.h - b.h)) < 0.5 * Math.abs (a.h + b.h)));
+		},
+
+		i: function (i)
+		{
+			let image = (typeof (i) == 'object') ? i : game.i[i];
+				image = (image) ? image : new Image ();
+			return image;
+		},
+
+		set images (i)
+		{
+			for (let n of i)
+			{
+				let image = new Image ();
+					image.src = 'data/' + n + '.png';
+				game.i[n] = image;
+			}
 		}
 	},
 
+	i: {},
 	id: 0,
 
 	load: function ()
@@ -175,10 +262,24 @@ game =
 
 window.onload = game.load;
 
+//load res
+game.get.animations =
+{
+	'test': 6
+}
+
+game.get.images =
+[
+	'test'
+]
+
+//game scenes
 game.scene.load = function ()
 {
 	game.create.box ({ color: '#f00', h: 100, w: 100, x: 100, y: 100, z: 2 }).load ();
 	game.create.box ({ color: '#0f0', h: 100, w: 100, x: 80, y: 130, z: 1 }).load ();
-	game.create.box ({ color: '#00f', h: 100, w: 100, x: 120, y: 160, z: 0 }).load ();
+	game.create.box ({ color: '#00f', h: 100, w: 100, x: 120, y: 160, z: 3 }).load ();
+	game.create.sprite ({ h: 100, i: 'test', w: 100, x: 200, y: 160, z: 0 }).load ();
+	game.create.animation ({ a: game.a.test, delay: 1000,  h: 100, i: 'test', loop: () => 1, w: 100, x: 100, y: 70, z: 2 }).load ();
 	game.draw ();
 }
